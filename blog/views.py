897d7ffa7840480c6a70baccc2d639c3f37a8fd4
yusufsacid <yusufsacid42@gmail.com>
2020-03-07 18:17:55 +0300

    css ekle :)

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
 from django.utils import timezone
-
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.order_by('published_date')
@@ -14,6 +14,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required(login_url='/admin')
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST or None)
